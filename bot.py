6bde9c048e63ea9cadafa974b7aa361dad7f7529
zakka00 <zaka.laoud.2004@gmail.com>
2023-05-01 02:07:34 +0200

    Terminato bot Telegram
    Aggiunta la funzionalità multichat e fatto in modo che venissero suggeriti : AgeRating, Industry, Director e writer in base a delle ricerche sulle liste di hashmap in memoria.
    Testato il tutto su due dispositivi diversi e worka come dovrebbe

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 import telebot
 from keys import *
 import Service
@@ -7,9 +6,8 @@ import json
 
 bot = telebot.TeleBot(token=chiave)
 
-
-film_data = {}
-Number_of_data = 0
+startedChats = []
+chatsInformation = []
 needed_data = ['IMDb Rating','Age Rating','Industry','Release date','Director','Writer','Language','Duration']
 ageRating = {'TV-PG': 0, 'R': 1, 'Unrated': 2, 'PG-13': 3, 'TV-MA': 4, 'TV-G': 5, 'TV-14': 6, 'PG': 7, 'TV-Y7': 8, 'G': 9, 'NC-17': 10, 'TV-Y': 11, 'Approved': 12, 'TV-Y7-FV': 13, 'MA-17': 14, 'TV-13': 15, 'Drama': 16, 'Drama, Romance': 17, 'Passed': 18, '18+': 19}
 industry = {'Hollywood / English': 0, 'Tollywood': 1, 'Wrestling': 2, 'Bollywood / Indian': 3, 'Punjabi': 4, 'Anime / Kids': 5, 'Dub / Dual Audio': 6, 'Pakistani': 7, 'Stage shows': 8, '3D Movies': 9}
@@ -18,11 +16,18 @@ industry = {'Hollywood / English': 0, 'Tollywood': 1, 'Wrestling': 2, 'Bollywood
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    global Number_of_data
-    print(message.text)
+    global startedChats
+    situationIndex = 0
+    print(message.chat.id)
+    if message.chat.id not in startedChats:
+        startedChats.append(message.chat.id)
+        chatsInformation.append({"chatid":message.chat.id, "N":0, "data":{}})
+    situationIndex = startedChats.index(message.chat.id)
+    chat_talking_with_me = chatsInformation[situationIndex]
     msg = message.text
     if ( 'start' in message.text ):
-        bot.reply_to(message, "Welcome! \nPlease, Enter the data relating to the film whose views you want to know\nStart by entering the IMDb Rating")
+        bot.reply_to(message, "Welcome! \nPlease, Enter the data relating the film whose views you want to predict")
+        bot.reply_to(message, "Start by entering the IMDb Rating")
     elif ( 'help' in message.text ):
         response = 'The informations that you should enter are:\n'
         for i in needed_data:
@@ -31,7 +36,7 @@ def echo_all(message):
     else:
         errors = False
         #Tutti i vari controlli
-        if Number_of_data == 0:
+        if chat_talking_with_me["N"] == 0:
             # controllo IMDb Rating
             try:
                 rating = float(msg)
@@ -40,65 +45,81 @@ def echo_all(message):
                     bot.reply_to(message, "The IMDb Rating should be between 0 and 10.\nPlease retry")
                     errors = True
                 else:
-                    film_data["imdb"] = msg
-                    print(film_data)
-                    Number_of_data+=1
+                    chat_talking_with_me["data"]["imdb"] = msg
+                    chat_talking_with_me["N"]+=1
             except:
                 bot.reply_to(message, "The IMDb Rating should be a number. Not a string or else.\nPlease retry")
                 errors = True
-        elif Number_of_data == 1:
+        elif chat_talking_with_me["N"] == 1:
             # controllo sull'age Rating
-            print(msg)
             if msg not in ageRating.keys():
-                bot.reply_to(message, "You've inserted an age Rating that does not exist.\nPlease retry")
+                possibleRating = ""
+                for code in ageRating.keys():
+                    possibleRating+=code + '\n'
+                bot.reply_to(message, "You've inserted an age Rating that does not exist.")
+                bot.reply_to(message,"Please try with one of there:\n" + possibleRating)
                 errors = True
             else:
-                film_data["age"] = Service.Predicter().ageRating[msg]
-
-
-                Number_of_data+=1
-        elif Number_of_data == 2:
+                chat_talking_with_me["data"]["age"] = Service.Predicter().ageRating[msg]
+                chat_talking_with_me["N"]+=1
+        elif chat_talking_with_me["N"] == 2:
             # controllo sull'idustria
             if msg not in industry.keys():
-                bot.reply_to(message, "You've inserted an industry that does not exist.\nPlease retry")
+                possibleIndustries = ""
+                for code in industry.keys():
+                    possibleIndustries+=code + '\n'
+                bot.reply_to(message, "You've inserted an industry that does not exist.")
+                bot.reply_to(message,"Please try with one of there:\n" + possibleIndustries)
                 errors = True
             else:
-                film_data["ind"] = Service.Predicter().industry[msg]
-                Number_of_data+=1
-        elif Number_of_data == 3:
+                chat_talking_with_me["data"]["ind"] = Service.Predicter().industry[msg]
+                chat_talking_with_me["N"]+=1
+        elif chat_talking_with_me["N"] == 3:
             # controllo sulla data d'uscita
             try:
                 date = datetime.strptime(msg, '%Y-%m-%d').date().year
-                film_data["date"] = msg
-                Number_of_data+=1
+                chat_talking_with_me["data"]["date"] = msg
+                chat_talking_with_me["N"]+=1
             except:
                 bot.reply_to(message, "You've mistanken the date.\nPlease retry")
                 errors = True
-        elif Number_of_data == 4:
+        elif chat_talking_with_me["N"] == 4:
             # controllo sul director
             if msg not in Service.Predicter().director:
-                bot.reply_to(message, "You've inserted a director that does not exist.\nPlease retry")
+                possibleDirectors = ""
+                for code in Service.Predicter().director.keys():
+                    if msg in code:
+                        possibleDirectors+=code + '\n'
+                bot.reply_to(message, "You've inserted a director that does not exist.")
+                if possibleDirectors != "":
+                    bot.reply_to(message,"Please try with one of there:\n" + possibleDirectors)
                 errors = True
             else:
-                film_data["dir"] = msg
-                Number_of_data+=1
-        elif Number_of_data == 5:
+                chat_talking_with_me["data"]["dir"] = msg
+                chat_talking_with_me["N"]+=1
+        elif chat_talking_with_me["N"] == 5:
             # controllo sul writer
-            if msg not in Service.Predicter().director:
-                bot.reply_to(message, "You've inserted a writer that does not exist.\nPlease retry")
+            if msg not in Service.Predicter().writer:
+                possibleWriters = ""
+                for code in Service.Predicter().director.keys():
+                    if msg in code:
+                        possibleWriters+=code + '\n'
+                bot.reply_to(message, "You've inserted a writer that does not exist.")
+                if possibleWriters != "":
+                    bot.reply_to(message,"Please try with one of there:\n" + possibleWriters)
                 errors = True
             else:
-                film_data["wri"] = msg
-                Number_of_data+=1
-        elif Number_of_data == 6:
+                chat_talking_with_me["data"]["wri"] = msg
+                chat_talking_with_me["N"]+=1
+        elif chat_talking_with_me["N"] == 6:
             # controllo sulla ligua
             if msg not in Service.Predicter().language:
                 bot.reply_to(message, "You've inserted a language that does not exist.\nPlease retry")
                 errors = True
             else:
-                film_data["lan"] = msg
-                Number_of_data+=1
-        elif Number_of_data == 7:
+                chat_talking_with_me["data"]["lan"] = msg
+                chat_talking_with_me["N"]+=1
+        elif chat_talking_with_me["N"] == 7:
             # controllo sulla durata
             try:
                 rating = int(msg)
@@ -106,32 +127,30 @@ def echo_all(message):
                     bot.reply_to(message, "The duration should be greater than 0.\nPlease retry")
                     errors = True
                 else:
-                    film_data["dur"] = msg
-                    Number_of_data+=1
+                    chat_talking_with_me["data"]["dur"] = msg
+                    chat_talking_with_me["N"]+=1
             except:
                 bot.reply_to(message, "The duration should be a number. Not a string or else.\nPlease retry")
                 errors = True
 
 
-        if Number_of_data == len(needed_data):
+        if chat_talking_with_me["N"] == len(needed_data):
             bot.reply_to(message, "Whait. I'm calculating the views that it is going to get if it was pirated by someone in the internet")
-            responce = Service.Predicter().predict(film_data)
-            print("stocco")
-            print(responce[0])
+            responce = Service.Predicter().predict(chat_talking_with_me["data"])
             views = int(responce[0])
             bot.reply_to(message, "Based on my calculations your title is going to get " + str(views) + " veiws")
-            Number_of_data = 0
-            film_data.clear()
+            bot.reply_to(message,"If you what to predict the views of another film just start by entering the IMDb rating")
+            chat_talking_with_me["N"] = 0
+            chat_talking_with_me["data"].clear()
         else:
             if errors == False:
-                res = "Good. Now enter " + needed_data[Number_of_data]
-                if Number_of_data == 3:
+                res = "Good. Now enter " + needed_data[chat_talking_with_me["N"]]
+                if chat_talking_with_me["N"] == 3:
                     res+="\nReamember that the date should be written by using this format (YYYY-mm-dd)"
                 bot.reply_to(message,res)
         errors = False
-            
 
-    
+
 bot.infinity_polling()
 
         
